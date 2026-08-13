0d27012fec1027501db775d7796ecce0d37a1d93
viniizn <jovini1303@gmail.com>
2026-08-13 08:35:13 -0300

    add request DTO

---
+++ backend/src/main/java/com/sentinela/Sentinela/dto/MetricsRequest.java
@@ -0,0 +1,27 @@
+package com.sentinela.Sentinela.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class MetricsRequest {
+
+    @NotBlank
+    private String hostname;
+    private String ip;
+    private String operatingSystem;
+
+    @NotNull
+    private Double cpu;
+
+    @NotNull
+    private Double ram;
+
+    @NotNull
+    private Double disk;
+
+    private Double temperature;
+    private Double networkRx;
+    private Double networkTx;
+}
